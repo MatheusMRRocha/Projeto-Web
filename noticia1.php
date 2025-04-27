45be40b9181fc3029132ba37925e1e67486aba5b
Wagner-05 <wagnermartins54312@gmail.com>
2025-04-27 17:16:21 -0300

    Add files via upload

---
+++ noticia1.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="style.css">
+
+<div class="container">
+    <h1>God of War Ragnarok: Nova expansão anunciada!</h1>
+
+    <img src="https://upload.wikimedia.org/wikipedia/en/7/7a/God_of_War_Ragnarok_cover.jpg" alt="God of War Ragnarok" style="width:100%; border-radius:10px; margin-top:20px;">
+
+    <p>Santa Monica Studio surpreendeu os fãs ao anunciar "Valhalla", a nova expansão de God of War Ragnarok. A aventura traz Kratos enfrentando novas ameaças mitológicas e explorando novos reinos.</p>
+
+    <p>Esta expansão promete mais de 20 horas de conteúdo extra, novos equipamentos, desafios e missões secundárias para os jogadores explorarem.</p>
+
+    <div style="text-align: center; margin-top: 30px;">
+        <a href="index.php">
+            <button>Voltar para a Home</button>
+        </a>
+    </div>
+</div>
---
+++ noticia4.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="style.css">
+
+<div class="container">
+    <h1>The Last of Us Parte III: Produção confirmada!</h1>
+
+    <img src="https://upload.wikimedia.org/wikipedia/en/a/a9/The_Last_of_Us_Part_II.jpg" alt="The Last of Us" style="width:100%; border-radius:10px; margin-top:20px;">
+
+    <p>A Naughty Dog confirmou que The Last of Us Part III está oficialmente em produção. Poucos detalhes foram revelados, mas rumores apontam que o novo jogo trará novos personagens e aprofundará ainda mais o mundo pós-apocalíptico.</p>
+
+    <p>Neil Druckmann, diretor criativo, afirmou que o roteiro está sendo desenvolvido com foco em temas de redenção, sobrevivência e esperança. Ainda não há previsão oficial de lançamento.</p>
+
+    <div style="text-align: center; margin-top: 30px;">
+        <a href="index.php">
+            <button>Voltar para a Home</button>
+        </a>
+    </div>
+</div>
