847e42d4f00c0c648c8e56c0045c50cc93c96f6e
MatheusMRRocha <matheusmreisrocha@gmail.com>
2025-04-27 17:48:54 -0300

    Noticias

--- index.php
+++ index.php
@@ -10,7 +10,13 @@
         <?php include 'html/cabecalho.php'; ?>
 
         <main class="conteudo">
-            <!-- Mesmo vazio, esse espaço vai crescer para empurrar o rodapé -->
+        <h1>Portal de Notícias de Jogos</h1>
+            <div class="news-links">
+                <p><a href="html/noticia1.php">God of War Ragnarok: Nova expansão anunciada!</a></p>
+                <p><a href="html/noticia2.php">FIFA 26: EA revoluciona o Modo Carreira</a></p>
+                <p><a href="html/noticia3.php">Cyberpunk 2077: Phantom Liberty já está disponível</a></p>
+                <p><a href="html/noticia4.php">The Last of Us Parte III: Produção confirmada!</a></p>
+            </div>
         </main>
 
         <?php include 'html/rodape.php'; ?>
