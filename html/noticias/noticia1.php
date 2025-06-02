037ed33005db546b04113f26aca39d24eead687b
MatheusMRRocha <matheusmreisrocha@gmail.com>
2025-06-02 11:58:12 -0300

    Update noticia1.php

--- html/noticias/noticia1.php
+++ html/noticias/noticia1.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>God of War Ragnarok</title>
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../../css/style.css">
 </head>
 <body>
 
 <div class="container">
+    <?php include '../cabecalho.php'; ?>
     <h1>God of War Ragnarok: Nova expansão anunciada!</h1>
 
-    <img src="https://upload.wikimedia.org/wikipedia/en/7/7a/God_of_War_Ragnarok_cover.jpg" alt="God of War Ragnarok" style="width:100%; border-radius:10px; margin-top:20px;">
+    <img src="https://pt.wikipedia.org/wiki/Ficheiro:God_of_War_Ragnar%C3%B6k_capa.jpg" alt="God of War Ragnarok" style="width:100%; border-radius:10px; margin-top:20px;">
 
     <p>Santa Monica Studio surpreendeu os fãs ao anunciar "Valhalla", a nova expansão de God of War Ragnarok. A aventura traz Kratos enfrentando novas ameaças mitológicas e explorando novos reinos.</p>
 
@@ -22,6 +23,7 @@
             <button>Voltar para a Home</button>
         </a>
     </div>
+    <?php include '../rodape.php'; ?>
 </div>
 
 </body>
