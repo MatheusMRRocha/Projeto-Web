833f47e8c3178290fe686d6896263c382d5571de
MatheusMRRocha <matheusmreisrocha@gmail.com>
2025-06-15 16:15:22 -0300

    noticia4
    estilização

--- html/noticias/noticia4.php
+++ html/noticias/noticia4.php
@@ -3,30 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="The Last of Us" href="../css/style.css">
+    <title>The Last of Us Parte III: Produção confirmada!</title>
+    <link rel="stylesheet" href="../../css/style.css">
 </head>
-<body>
-    
-<div class="container">
-    <?php include '../cabecalho.php'; ?>
-    
-    <h1>The Last of Us Parte III: Produção confirmada!</h1>
+    <div class="container">
+        <?php include '../cabecalho.php'; ?>
 
-    <img src="../../img/The_Last_Of_Us_Parte_II.jpg" alt="The Last of Us" style="width: 50%; border-radius:10px; margin-top:20px;">
+        <main class="conteudo">
+            <h1>The Last of Us Parte III: Produção confirmada!</h1>
 
-    <p>A Naughty Dog confirmou que The Last of Us Part III está oficialmente em produção. Poucos detalhes foram revelados, mas rumores apontam que o novo jogo trará novos personagens e aprofundará ainda mais o mundo pós-apocalíptico.</p>
+            <img src="../../img/The_Last_Of_Us_Parte_II.jpg" alt="The Last of Us"style="width:100%; border-radius:10px; margin-top:20px; display: block; margin-left: auto; margin-right: auto; max-width: 800px;">
+">
 
-    <p>Neil Druckmann, diretor criativo, afirmou que o roteiro está sendo desenvolvido com foco em temas de redenção, sobrevivência e esperança. Ainda não há previsão oficial de lançamento.</p>
+            <p>A Naughty Dog confirmou que The Last of Us Part III está oficialmente em produção. Poucos detalhes foram revelados, mas rumores apontam que o novo jogo trará novos personagens e aprofundará ainda mais o mundo pós-apocalíptico.</p>
 
-    <div style="text-align: center; margin-top: 30px;">
-        <a href="../../index/index.php">
-            <button>Voltar para a Home</button>
-        </a>
+            <p>Neil Druckmann, diretor criativo, afirmou que o roteiro está sendo desenvolvido com foco em temas de redenção, sobrevivência e esperança. Ainda não há previsão oficial de lançamento.</p>
+
+            <div style="text-align: center; margin-top: 30px;">
+                <a href="../../index/index.php">
+                    <button style="
+                        background-color: #3f51b5;
+                        color: white;
+                        padding: 10px 20px;
+                        border: none;
+                        border-radius: 5px;
+                        cursor: pointer;
+                        font-size: 16px;
+                        transition: background-color 0.3s ease;
+                    ">Voltar para a Home</button>
+                </a>
+            </div>
+        </main>
+
+        <?php include '../rodape.php'; ?>
     </div>
-
-    <?php include '../rodape.php'; ?> </div>
-</div>
-
 </body>
 </html>
