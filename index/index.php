30cf9e6100803d48f58ed587a9912d9c7fd5956e
MatheusMRRocha <matheusmreisrocha@gmail.com>
2025-06-15 19:20:52 -0300

    Index Atualizado

--- index/index.php
+++ index/index.php
@@ -19,22 +19,18 @@ session_start(); // Inicia a sessão para que o cabeçalho possa verificar o sta
             <div class="news-links">
                 <h2>Últimas Notícias</h2>
                 <ul class="latest-news-list">
-                    <li><a href="../html/noticias/noticia.php?id=1">God of War Ragnarok: Nova expansão anunciada!</a></li>
-                    <li><a href="../html/noticias/noticia.php?id=2">FIFA 26: EA revoluciona o Modo Carreira</a></li>
-                    <li><a href="../html/noticias/noticia.php?id=3">Cyberpunk 2077: Phantom Liberty já está disponível</a></li>
-                    <li><a href="../html/noticias/noticia.php?id=4">The Last of Us Parte III: Produção confirmada!</a></li>
-                    <li><a href="../html/noticias/noticia.php?id=8">High On Life 2: Anunciado com Mais Humor e Ação!</a></li>
-                    <li><a href="../html/noticias/noticia.php?id=10">Titanfall 3: CEO da Respawn Deixa Fãs Esperançosas!</a></li>
-                    <li><a href="../html/noticias/noticia.php?id=9">Minecraft 2: Anúncio Gera Expectativa Apesar de Cancelamento</a></li>
-                    <li><a href="../html/noticias/noticia.php?id=6">Forza Horizon 6: Brasil como Mapa Confirmado!</a></li>
-                    <li><a href="../html/noticias/noticia.php?id=5">Baldur’s Gate 3: Consagrado o Melhor RPG de 2024!</a></li>
-                    <li><a href="../html/noticias/noticia.php?id=7">Dragon Age: Dreadwolf promete revolução no sistema de escolhas</a></li>
+                    <li><a href="../html/noticias/noticia1.php">God of War Ragnarok: Nova expansão anunciada!</a></li>
+                    <li><a href="../html/noticias/noticia2.php">FIFA 26: EA revoluciona o Modo Carreira</a></li>
+                    <li><a href="../html/noticias/noticia3.php">Cyberpunk 2077: Phantom Liberty já está disponível</a></li>
+                    <li><a href="../html/noticias/noticia4.php">The Last of Us Parte III: Produção confirmada!</a></li>
+                    <li><a href="../html/noticias/noticia5.php">High On Life 2: Anunciado com Mais Humor e Ação!</a></li>
+                    <li><a href="../html/noticias/noticia6.php">Titanfall 3: CEO da Respawn Deixa Fãs Esperançosas!</a></li>
+                    <li><a href="../html/noticias/noticia7.php">Minecraft 2: Anúncio Gera Expectativa Apesar de Cancelamento</a></li>
+                    <li><a href="../html/noticias/noticia8.php">Forza Horizon 6: Brasil como Mapa Confirmado!</a></li>
+                    <li><a href="../html/noticias/noticia9.php">Baldur’s Gate 3: Consagrado o Melhor RPG de 2024!</a></li>
+                    <li><a href="../html/noticias/noticia10.php">Dragon Age: Dreadwolf promete revolução no sistema de escolhas</a></li>
                 </ul>
             </div>
-            
-            <h2>Categorias</h2>
-            <button id="btn-categorias">Mostrar Categorias</button>
-            <ul id="lista-categorias"></ul>
         </main>
 
         <?php include '../html/rodape.php'; ?> 
