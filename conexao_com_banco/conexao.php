7007eae4b82356ea9379d583787349eef1e0467f
Wagner-05 <wagnermartins54312@gmail.com>
2025-06-15 12:27:07 -0300

    Add files via upload

---
+++ conexao_com_banco/conexao.php
@@ -0,0 +1,20 @@
+<?php
+$host = 'localhost'; 
+$db = 'your_database_name';
+$user = 'your_username'; 
+$pass = 'your_password'; 
+$port = '5432'; 
+
+$dsn = "pgsql:host=$host;port=$port;dbname=$db";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+?>
