bfb8049f137075a2b3813c29ad2ad4831525bad8
MatheusMRRocha <matheusmreisrocha@gmail.com>
2025-06-15 19:58:45 -0300

    Finalizado

--- html/rodape.php
+++ html/rodape.php
@@ -1,5 +1,5 @@
 <footer class="rodape">
     <p>&copy; 2025 - Todos os direitos reservados.</p>
     <hr style="border: none; border-top: 1px solid rgba(255, 255, 255, 0.1); margin: 10px auto; width: 80%;">
-    <p>Contato: gamenewsgames@gmail.com</p>
+    <p>Contato: gamenews@gmail.com (email ficticio)</p>
 </footer>
