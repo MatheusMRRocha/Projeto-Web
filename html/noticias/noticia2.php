51c3f5c95f057e05b55b366b0fb9fce1e404c13c
MatheusMRRocha <matheusmreisrocha@gmail.com>
2025-06-15 16:03:47 -0300

    Links

--- html/noticias/noticia2.php
+++ html/noticias/noticia2.php
@@ -10,21 +10,21 @@
     
 <div class="container">
     <?php include '../cabecalho.php'; ?>
+    <main class="conteudo">
+        <h1>FIFA 26: EA revoluciona o Modo Carreira</h1>
 
-    <h1>FIFA 26: EA revoluciona o Modo Carreira</h1>
+        <img src="../../img/fifa26.jpg" alt="FIFA" style="width:100%; border-radius:10px; margin-top:20px;">
 
-    <img src="../img/fifa26.jpg" alt="FIFA" style="width:100%; border-radius:10px; margin-top:20px;">
+        <p>O Modo Carreira do FIFA 26 será completamente reformulado, segundo a EA Sports. Agora, jogadores poderão realizar entrevistas ao vivo, participar de eventos sociais e ver seus atletas evoluírem com inteligência artificial dinâmica.</p>
 
-    <p>O Modo Carreira do FIFA 26 será completamente reformulado, segundo a EA Sports. Agora, jogadores poderão realizar entrevistas ao vivo, participar de eventos sociais e ver seus atletas evoluírem com inteligência artificial dinâmica.</p>
+        p>A EA promete também melhorias gráficas, movimentação mais realista e um sistema de escolhas que afetará diretamente o rumo da carreira. FIFA 26 será lançado em setembro para múltiplas plataformas.</p>
 
-    <p>A EA promete também melhorias gráficas, movimentação mais realista e um sistema de escolhas que afetará diretamente o rumo da carreira. FIFA 26 será lançado em setembro para múltiplas plataformas.</p>
-
-    <div style="text-align: center; margin-top: 30px;">
-        <a href="../../index/index.php"">
-            <button>Voltar para a Home</button>
-        </a>
-    </div>
-
+        <div style="text-align: center; margin-top: 30px;">
+            <a href="../../index/index.php"">
+                <button>Voltar para a Home</button>
+            </a>
+        </div>
+    </main>
     <?php include '../rodape.php'; ?> </div>
 </div>
 
